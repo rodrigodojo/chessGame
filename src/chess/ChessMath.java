fdec632e9f32dc75a2c4f074c925dcc9296b734f
rodrigodojo <rodrigodoj@gmail.com>
2021-02-03 12:43:28 -0300

    adicionado logica de contagem de movimento

--- src/chess/ChessMath.java
+++ src/chess/ChessMath.java
@@ -15,7 +15,7 @@ public class ChessMath {
 	private Color currentPlayer;
 	private boolean check;
 	private boolean checkMate;
-	
+		
 	private List<Piece> piecesOnTheBoard = new ArrayList<>();
 	private List<Piece> capturedPieces = new ArrayList<>();
 	
@@ -83,7 +83,8 @@ public class ChessMath {
 	}
 	
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p =(ChessPiece) board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		if(capturedPiece != null) {
@@ -95,7 +96,8 @@ public class ChessMath {
 	
 	private void undoMake(Position source, Position target, Piece capturedPiece) {
 		
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece)board.removePiece(target);
+		p.decreaseMoveCount();
 		board.placePiece(p, source);
 		
 		if(capturedPiece != null) {
